0d090fefd6b6048e1ba1a32e0d1e4b888cce34b7
Bayu Prayogo <Bayuaria33@gmail.com>
2023-02-18 17:44:18 +0700

    added cors, added missing route handler

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
 const express = require("express");
 require('dotenv').config()
 const bodyParser = require("body-parser");
+const cors = require("cors");
+
 const app = express();
 const port = 3000;
 const mainRoute = require(`./src/routes/index`)
+app.use(cors({
+  origin: "*",
+  method:"*"
+}));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use('/', mainRoute);
 
+app.get("/", (req, res, next) => {
+  res.status(200).json({ status: "success", statusCode: 200 });
+});
 
+//404 on missing route
+app.all("*",(req,res,next)=> {
+  res.status(404).json({
+    message: `404 Main Route Not Found`
+  })
+})
 
 // console.log(process.env.DB_NAME);
 app.listen(port, () => {
