318542b5326c6bbde5871cda2e426199a316cc10
Bayu Prayogo <Bayuaria33@gmail.com>
2023-02-19 15:44:26 +0700

    added search, sort, softdelete to categ model

--- src/model/categoryModel.js
+++ src/model/categoryModel.js
@@ -1,12 +1,13 @@
 const { query } = require("express");
 const Pool = require("./../config/dbconfig");
 
-const selectDataCategory = () => {
-  return Pool.query(`SELECT * FROM categories`);
+const selectDataCategory = (data) => {
+  let { searchBy, search, sortBy, sort, limit, offset } = data;
+  return Pool.query(`SELECT id, category_name FROM categories WHERE categories.${searchBy} ILIKE '%${search}%' AND categories.deleted_at IS NULL ORDER BY categories.${sortBy} ${sort} LIMIT ${limit} OFFSET ${offset}`);
 };
 
 const selectDataCategoryById = (data) => {
-  let qry = `SELECT * FROM categories WHERE id='${data}'`;
+  let qry = `SELECT * FROM categories WHERE id='${data}' AND deleted_at IS NULL`;
   // console.log(data, query);
   return Pool.query(qry);
 };
@@ -26,7 +27,8 @@ const updateDataCategory = (id, data) => {
 };
 
 const deleteDataCategory = (id) => {
-  let qry = `DELETE FROM categories WHERE id=${id}`;
+  // let qry = `DELETE FROM categories WHERE id=${id}`;
+  let qry = `UPDATE categories SET deleted_at = NOW()::timestamp WHERE id=${id}`;
   console.log(id, qry);
   return Pool.query(qry);
 };
