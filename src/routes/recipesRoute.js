ea2547f560d23908b8877446a8e0a7f7eec2a0d6
Bayu Prayogo <Bayuaria33@gmail.com>
2023-03-08 11:27:15 +0700

    temporarily removed auth protect for dev purposes

--- src/routes/recipesRoute.js
+++ src/routes/recipesRoute.js
@@ -7,12 +7,18 @@ const {
   putDataRecipe,
   deleteDataRecipe,
 } = require("./../controller/recipesCon");
-const { protect } = require("../middleware/auth");
+// const { protect } = require("../middleware/auth");
 const { upload } = require("../middleware/upload");
 
-router.get("/", protect, getAllRecipes);
-router.get("/my-recipe", protect, getRecipeById);
-router.post("/", protect, upload.single("photo"), postDataRecipe);
-router.put("/:id", protect, upload.single("photo"), putDataRecipe);
-router.delete("/:id", protect, deleteDataRecipe);
+// router.get("/", protect, getAllRecipes);
+// router.get("/my-recipe", protect, getRecipeById);
+// router.post("/", protect, upload.single("photo"), postDataRecipe);
+// router.put("/:id", protect, upload.single("photo"), putDataRecipe);
+// router.delete("/:id", protect, deleteDataRecipe);
+
+router.get("/", getAllRecipes);
+router.get("/my-recipe", getRecipeById);
+router.post("/", upload.single("photo"), postDataRecipe);
+router.put("/:id", upload.single("photo"), putDataRecipe);
+router.delete("/:id", deleteDataRecipe);
 module.exports = router;
--- src/routes/usersRoute.js
+++ src/routes/usersRoute.js
@@ -1,16 +1,23 @@
 const express = require("express");
 const router = express.Router();
-const {protect} = require('../middleware/auth')
+// const {protect} = require('../middleware/auth')
 const {
   getAllUsers,
   getUserById,
   putDataUser,
 } = require("./../controller/usersCon");
 
+// router.get("/", getAllUsers);
+// router.get("/myprofile",protect, getUserById);
+// // router.post("/", postDataUser);
+//  router.put("/update",protect, putDataUser);
+// // router.delete("/:id", deleteDataUser);
+
 router.get("/", getAllUsers);
-router.get("/myprofile",protect, getUserById);
+router.get("/myprofile", getUserById);
 // router.post("/", postDataUser);
- router.put("/update",protect, putDataUser);
+ router.put("/update", putDataUser);
 // router.delete("/:id", deleteDataUser);
+
 
 module.exports = router;
