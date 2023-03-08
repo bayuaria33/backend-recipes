1474b73d2b9572e5dbff50e462d20b88f30d2a84
Bayu Prayogo <Bayuaria33@gmail.com>
2023-03-08 11:33:50 +0700

    reverted previous commit lol

--- src/routes/usersRoute.js
+++ src/routes/usersRoute.js
@@ -1,22 +1,16 @@
 const express = require("express");
 const router = express.Router();
-// const {protect} = require('../middleware/auth')
+const {protect} = require('../middleware/auth')
 const {
   getAllUsers,
   getUserById,
   putDataUser,
 } = require("./../controller/usersCon");
 
-// router.get("/", getAllUsers);
-// router.get("/myprofile",protect, getUserById);
-// // router.post("/", postDataUser);
-//  router.put("/update",protect, putDataUser);
-// // router.delete("/:id", deleteDataUser);
-
 router.get("/", getAllUsers);
-router.get("/myprofile", getUserById);
+router.get("/myprofile",protect, getUserById);
 // router.post("/", postDataUser);
- router.put("/update", putDataUser);
+ router.put("/update",protect, putDataUser);
 // router.delete("/:id", deleteDataUser);
 
 
