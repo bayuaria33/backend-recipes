340e1050db65af8c5dc01c68a42cda480bd14020
Bayu Prayogo <Bayuaria33@gmail.com>
2023-03-20 10:35:51 +0700

    tried fixing mailer

--- src/middleware/email.js
+++ src/middleware/email.js
@@ -1,32 +1,44 @@
-const nodemailer = require("nodemailer")
-
+const nodemailer = require("nodemailer");
 
 let transporter = nodemailer.createTransport({
-    host: "smtp.zoho.com",
-    secure: true,
-    port: 465,
-    auth: {
-      user: process.env.EMAIL_NAME,
-      pass: process.env.EMAIL_PASSWORD
-    },
+  host: "smtp.zoho.com",
+  secure: true,
+  port: 465,
+  auth: {
+    user: process.env.EMAIL_NAME,
+    pass: process.env.EMAIL_PASSWORD,
+  },
+});
+
+module.exports = (emailClient, subject, url, name) => {
+  let mailOption = {
+    from: process.env.EMAIL_NAME,
+    to: emailClient,
+    subject: `${subject} is your otp`,
+    text: `Hello ${name}, ${subject} is your otp, please input in form ${url}`,
+  };
+
+  new Promise((resolve, reject) => {
+    transporter.sendMail(mailOption, function (error, data) {
+      if (error) {
+        reject(error.message);
+      } else {
+        resolve(data);
+      }
+    });
+  }).then((data) => {
+    console.log("Email sent successfully");
+    return data;
   });
 
-  module.exports = (emailClient,subject,url,name) => {
-    let mailOption ={
-        from:  process.env.EMAIL_NAME,
-        to: emailClient,
-        subject:`${subject} is your otp`,
-        text: `Hello ${name}, ${subject} is your otp, please input in form ${url}`
-    }
-
-    transporter.sendMail(mailOption,function(error,data){
-        if(error){
-            console.log("error : ", error)
-            return "Email not sent"
-        } else{
-            console.log("OTP email sent ")
-            console.log(data)
-            return "Email success"
-        }
-    })
-  }
+  // transporter.sendMail(mailOption,function(error,data){
+  //     if(error){
+  //         console.log("error : ", error)
+  //         return "Email not sent"
+  //     } else{
+  //         console.log("OTP email sent ")
+  //         console.log(data)
+  //         return "Email success"
+  //     }
+  // })
+};
